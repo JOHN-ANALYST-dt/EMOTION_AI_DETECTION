6e950b6bf960b9552ea5ff101d16d834101e6775
john <john.nyangoya@student.moringaschool.com>
2025-11-10 01:32:18 +0300

    addsw

--- app.py
+++ app.py
@@ -174,10 +174,10 @@ def predict_emotion(text):
 st.title("🧠 CareEmotion AI")
 st.markdown(
     """
-    <div style="text-align: center; color: black; font-size:50px; padding: 10px; border: 1px solid black; border-radius: 5px;font-weight: bold;">
+    <div style="text-align: center; color: black; font-size:50px; padding: 10px; border: 1px solid black; border-radius: 10px;font-weight: bold;">
         Understand Emotions in Text
         <h4>
-        Our AI model analyzes text to detect emotional undertones with precision.<br> Enter any text below to discover its emotional fingerprint.
+        Our AI model analyzes text to detect emotional undertones with precision. Enter any text below to discover its emotional fingerprint.
         </h4>
     </div>
     """, 
