c45c0834d2e9e334b0840e749866d3a764f8716d
john <john.nyangoya@student.moringaschool.com>
2025-11-12 01:04:11 +0300

    addgk

--- app.py
+++ app.py
@@ -182,16 +182,7 @@ def predict_emotion(text):
 
 # --- 4. Streamlit UI Design ---
 
-import base64
 
-def get_base64_image(image_path):
-    """Converts a local image file to base64 string."""
-    with open(image_path, "rb") as img_file:
-        encoded = base64.b64encode(img_file.read()).decode()
-    return f"data:image/jpeg;base64,{encoded}"
-
-# Load your local image and embed it into CSS
-bg_image = get_base64_image("images/happy2.jpg")
 
 
 
